3430faa3fcc7d2cf6d8e01b69bdecd2a8393cd44
Lukas Hezel <ljh0020@uah.edu>
2026-01-01 21:47:03 -0500

    fixed carousel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+/* Imports *********************************************************/
 import { useState } from 'react'
 import './App.css'
 
+/* Constants *******************************************************/
 const carImgs = [
   "/carousel/TwoMonth_bg1.JPG",
   "/carousel/TwoMonth_bg2.JPG",
   "/carousel/TwoMonth_bg3.JPG",
   "/carousel/TwoMonth_bg4.JPG",
-  "/carousel/TwoMonth_bg5.JPG"
+  "/carousel/TwoMonth_bg5.JPG",
+  "/carousel/TwoMonthCollage.jpg"
 ];
 const carAlts = [
   "This is image 1",
@@ -20,21 +23,40 @@ const carCaps = [
   "This is caption 2",
   "This is caption 3",
   "This is caption 4",
-  "This is caption 5"
+  "This is caption 5",
+  "A collage of images taken from an amateur high-altitude ballooning project I took part in."
 ]
 
+/* Main Export *****************************************************/
 export default function App() {
   const [count, setCount] = useState(0)
   const [carIdx, setCarImg] = useState(0)
   const next = () => setCarImg((carIdx + 1) % carImgs.length);
   const prev = () => setCarImg((carIdx - 1 + carImgs.length) % carImgs.length);
 
-
   return (
     <>
       <header>
         <h1>Lukas Hezel Portfolio</h1>
       </header>
+
+      <div className='landing'>
+        <p className='welcome'>
+          Welcome!
+        </p>
+        <p>
+          Thanks for visiting my personal portfolio webpage.
+          I made it using React and base CSS, and I don't think it's too shabby for a side project.
+          I'm using this page as a space to display the cool things I've done.
+          Feel free to contact me or check out my profiles in these links:
+        </p>
+        <div className='links'>
+          <a href="mailto:luke.hezel@gmail.com">Email</a>
+          <a href="https://www.linkedin.com/in/luke-hezel-83545628a">LinkedIn</a>
+          <a href="https://github.com/l-heze-l">Github</a>
+        </div>
+      </div>
+
       <div className='body'>
         <div className="carousel"> 
           <button onClick={prev}>‹</button> 
@@ -48,6 +70,7 @@ export default function App() {
           </div>
           <button onClick={next}>›</button> 
         </div>
+
         <div className="card">
           <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
@@ -55,30 +78,6 @@ export default function App() {
           <p>
             Work in Progress (as you can tell lol)...
           </p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-          
         </div>
       </div>
     </>
